080e9bfc0d08c3102c84d21e8fe80ded30688231
Omkar Patole <omkarpatole1799@gmail.com>
2023-07-16 14:02:53 +0530

    Linera search in 2D array

--- ds_algo/py/Searching/linearSearchArr.py
+++ ds_algo/py/Searching/linearSearchArr.py
@@ -5,4 +5,19 @@ def linearSearchArr(arr, target):
         if (arr[i] == target):
             return i
         
-print(linearSearchArr([1,3,11,3,22], 22))
+
+
+def linearSearch2DArr(arr, target):
+    colIndex = 0
+    rowIndex = 0
+    for i in range(len(arr)):
+        colIndex += 1
+        for j in arr[i]:
+            print(j)
+            if(arr[i][j] == target):
+                return colIndex, rowIndex
+
+
+if '__main__' == '__main__':
+    # print(linearSearchArr([1,3,11,3,22], 22))
+    print(linearSearch2DArr([[4,2,1], [1,2,3,10]], 10))
