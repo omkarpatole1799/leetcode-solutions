b56b14496e42454d9b4bc2d8bb6f90f7a1b12d8c
Omkar Patole <omkarpatole1799@gmail.com>
2023-07-17 09:15:34 +0530

    refactoring

---
+++ ds_algo/JS/BinarySearch/orderAgnosticBS.js
@@ -0,0 +1,34 @@
+function orderAgnosticBS(array, target){
+    let isAsc = true
+    let start = 0
+    let end = array.length - 1
+    if(array[0] > array[array.length -1]){
+        isAsc = false
+    }
+    while (start <= end){
+        let middleIndex = Math.floor((start + end) / 2)
+        if(isAsc){
+            if(target < array[middleIndex]){
+                end = end - 1
+            } else if(target > array[middleIndex]){
+                start = start + 1
+            } else {
+                return middleIndex
+            }
+        }
+        if (!isAsc) {
+            if (target < array[middleIndex]) {
+                end = end + 1
+            } else if (target > array[middleIndex]) {
+                start = start - 1
+            } else {
+                return middleIndex
+            }
+        }
+    }
+    return "not found"
+}
+
+console.log(orderAgnosticBS([1,3,4,5,6,7,8], 3))
+console.log(orderAgnosticBS([8,7,6,5,4,3,1], 3))
+
